0c8e37e2e9cb58a5bb4679b3ebedd53786aef7db
Karthik-Prabhu-kp <kpwat66@gmail.com>
2020-09-13 00:09:05 +0530

    changed switch case

--- EmpSwitch.java
+++ EmpSwitch.java
@@ -2,24 +2,20 @@ public class EmpSwitch
 {
 	public static void main(String[] args)
 	{
-	 int Is_full_time = 1;
-	 int Is_part_time = 2;
 	 int WagePerHr = 20;
-	 int full_time = 16;
-	 int part_time = 8;
-	 int min = 0;
-	 int max = 2;
 	 
-	 int empcheck = (int)(Math.floor(Math.random() * (max - min + 1) + min));
+	 int empcheck = (int)(Math.floor(Math.random()*3));
 
 	 switch (empcheck){
 	 case 1:
-	  		int totalwage = WagePerHr*full_time;
-			System.out.println("Wage="+totalwage);
+			int full_time=16;
+	  		int dailywage = WagePerHr*full_time;
+			System.out.println(dailywage);
 	 		break;
 	 case 2: 
-        	int totalwage = WagePerHr*part_time;
-            System.out.println("Wage="+totalwage);
+			int parttime=8;
+        	int totalwage = WagePerHr*parttime;
+            System.out.println(totalwage);
 	 		break;
 
 	 default:
